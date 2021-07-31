8548cfeb1016ea9d61400bdb1c423b69c5267cb0
Vincent Chiang <vincentchiang1@gmail.com>
2021-07-31 12:44:19 -0700

    set up resource card

--- pages/charcuterie.js
+++ pages/charcuterie.js
@@ -1,5 +1,12 @@
 import Head from 'next/head'
 
+const resourceCard = (name, event = null, year, image, badge = null, resourceTypeLogo) => (
+  <div>
+    <img src={image} width='295px' height='210px' />
+    <h3 style={{color: '#20FFAF'}}>{name}</h3>
+    <span>{event ? event + ' | ' + year : year}</span>
+  </div>
+)
 export default function Charcuterie() {
   return (
     <>
@@ -11,6 +18,7 @@ export default function Charcuterie() {
         <h1>charcuterie 😋</h1>
         <p>Just a place where we put things</p>
       </main>
+      {resourceCard('ligma', 'coachella', '2022', 'https://variety.com/wp-content/uploads/2021/03/coachella-music-festival.jpg')}
     </>
   )
 }
