75496406651123d61aac805723399f96535684fd
Phoenix <phoenix@zaber.com>
2021-08-02 11:52:24 -0700

    adding the nwplus logo

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,18 +1,29 @@
 import styled from 'styled-components'
+
+import nwplusLogo from 'nwPlus_Logo_2020.png'
  
 const NavBarContainer = styled.div`
 `;
 
-const NavLogoContainer = styled.div`
+const NavLogo = styled.img`
 `;
 
 const NavGroupContainer = styled.div`
+  display: flex;
+  gap: 46.22px;
+  align-items: center;
 `;
 
 const SimpleLink = styled.a`
+  appearance: none;
 `;
 
 const SimpleLinkText = styled.span`
+  font-family: HK Grotesk;
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 23px;
+  color: #FFFFFF;
 `;
 
 const LinkGroup = () => {
@@ -43,7 +54,7 @@ export const NavBar = () => {
   return (
     <NavBarContainer>
       <NavGroupContainer>
-        <NavLogoContainer></NavLogoContainer>
+        <NavLogo src={nwplusLogo} alt="nwPlus club logo in white against dark blue background"/>
         <SimpleLink href="#"><SimpleLinkText>
           About Us
         </SimpleLinkText></SimpleLink> 
