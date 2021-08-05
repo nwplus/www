e390a4e43cc8e1424be5b03fd57dd9ab105a1f94
Ian Mah <imah@tesla.com>
2021-08-05 00:22:27 -0700

    Starter footer team

--- components/Footer.js
+++ components/Footer.js
@@ -9,12 +9,46 @@ import {
   faYoutube,
 } from '@fortawesome/free-brands-svg-icons'
 
-const FooterContainer = styled.div`
-  background: #3A396E;
-  color: #20FFAF;
+const MOCK_PROFILE = {
+  img: 'https://www.pngall.com/wp-content/uploads/2016/06/Obama-Free-Download-PNG.png',
+  name: 'Travis Scott',
+  title: 'Founder of the travis scott burger'
+}
+
+const profiles = Array(20).fill(MOCK_PROFILE)
+
+const FooterContainer = styled.footer`
+  background: url(/assets/footer_bg.svg), ${p => p.theme.colors.background};
+  background-size: cover;
+  color: ${p => p.theme.colors.primary};
   text-align: center;
   padding: 64px 0;
 `;
+
+const Header = styled.h2`
+  font-weight: 500;
+  font-size: 1.9em;
+`
+
+const ProfileList = styled.div`
+  height: 140px;
+  overflow-x: auto;
+  white-space: nowrap;
+  scrollbar-height: none; 
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+const ProfileImage = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 12px;
+  background-color: red;
+  object-fit: cover;
+  margin: 0 1em;
+`
 
 const SocialMediaIcons = styled.div`
   font-size: 32px;
@@ -30,8 +64,15 @@ const SocialMediaIcons = styled.div`
 `;
 
 export default function Footer() {
+  console.log(profiles)
   return (
     <FooterContainer>
+      <Header>Meet the minds behind nwPlus</Header>
+      <ProfileList>
+        {profiles.map((profile) => (
+          <ProfileImage src={profile.img} />
+        ))}
+      </ProfileList>
       <SocialMediaIcons>
         <a href="https://www.facebook.com/nwplusubc" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faFacebook} />
