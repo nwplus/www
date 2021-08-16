71d579828821c553473e56146d06bcdab0bc9e49
Jenny Pan <panjenny0@gmail.com>
2021-08-15 21:28:34 -0700

    fixed image proportion issue and nit issue with svg

--- components/Hero.js
+++ components/Hero.js
@@ -2,16 +2,19 @@ import { ContentContainer } from './ContentContainer';
 import styled from 'styled-components';
 import { LargeTitle, Title2 } from './Typography';
 
+/** hack to display image without cropping: width=100%, height=0, padding-bottom to size the div to the image’s proportion
+ * padding-bottom: height / width = 998px / 1440px ≈ 69%
+ * https://www.quora.com/In-CSS-how-do-I-set-a-background-image-on-a-div-without-part-of-the-image-getting-cutoff */
 const HeroContainer = styled.div`
+  width: 100%;
+  height: 0;
+  padding-bottom: 69%;
   background: url(/assets/hero_illustration.svg);
   background-size: cover;
 `;
 
 const HeroTextContainer = styled.div`
-  padding: 480px 0 400px 0;
-  ${(p) => p.theme.mediaQueries.mobile} {
-    padding: 184px 0 160px 0;
-  }
+  padding-top: 33%;
 `;
 
 export default function Hero() {
