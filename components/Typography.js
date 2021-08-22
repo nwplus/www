b6d2668902a1e9e3a219d1ff5dd6a37711ab2e83
Your Name <new2282307660@gmail.com>
2021-08-22 10:43:32 -0700

    added typography for Title1

--- components/Typography.js
+++ components/Typography.js
@@ -16,6 +16,7 @@ export const LargeTitle = styled.h1.attrs((p) => ({
   font-weight: 900;
   font-size: 56.83px;
   letter-spacing: 0.04em;
+  line-height: 41px;
   ${SetColor}
   ${SetBackground}
   ${(p) =>
@@ -25,6 +26,28 @@ export const LargeTitle = styled.h1.attrs((p) => ({
     -webkit-text-fill-color: transparent;`}
   ${(p) => p.theme.mediaQueries.mobile} {
     font-size: 32px;
+  }
+`;
+
+export const Title1 = styled.h1.attrs((p) => ({
+  color: p.color || p.theme.colors.secondary,
+}))`
+  margin: 0;
+  font-style: normal;
+  font-weight: 800;
+  font-size: 42.63px;
+  letter-spacing: 0.38px;
+  line-height: 53.46px;
+  ${SetColor}
+  ${SetBackground}
+  ${(p) =>
+    p.withGradient &&
+    `background: -webkit-linear-gradient(92deg, #19cbcb 1.55%, #78ff96 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;`}
+  ${(p) => p.theme.mediaQueries.mobile} {
+    font-size: 21.32px;
+    line-height: 27.77px;
   }
 `;
 
