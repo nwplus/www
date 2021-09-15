48e8b38fb4c079263cde33991c71143992904dab
Kevin Zou <kevin.zp.zou@gmail.com>
2021-09-14 17:32:18 -0700

    Add more resources

--- utils/ResourceUtils.js
+++ utils/ResourceUtils.js
@@ -43,12 +43,52 @@ export const resourceCategoryMapper = {
 export const RESOURCES = [
   {
     name: 'Web Dev for Dummies',
-    event: ResourceEvent.NW_HACKS,
     year: '2020',
     image: 'https://i.ytimg.com/vi/R7UxJMAVvLU/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDWdHwEkVTw0mR8hmb0UwxMI0CcYQ',
     badge: BADGE_ICON,
     type: ResourceType.VIDEO,
     link: 'https://www.youtube.com/watch?v=R7UxJMAVvLU'
+  },
+  {
+    name: 'Git the Basics Workshop',
+    event: ResourceEvent.CMD_F,
+    year: '2021',
+    image: 'https://i.ytimg.com/an_webp/BjJn4HW_-PI/mqdefault_6s.webp?du=3000&sqp=CNzBhIoG&rs=AOn4CLCKvA-rF5Obce1IYBVBmWEdozlo7Q',
+    badge: BADGE_ICON,
+    type: ResourceType.VIDEO,
+    link: 'https://www.youtube.com/watch?v=BjJn4HW_-PI'
+  },
+  {
+    name: 'Technical Interview Workshop: Beginner Edition',
+    year: '2020',
+    image: 'https://i.ytimg.com/vi/J7Goi2aF0uw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCqoUcsCe75F6Krxl_QUpx9hZeGWQ',
+    badge: BADGE_ICON,
+    type: ResourceType.VIDEO,
+    link: 'https://www.youtube.com/watch?v=J7Goi2aF0uw'
+  },
+  {
+    name: 'Behind cmd-f 2021',
+    event: ResourceEvent.CMD_F,
+    year: '2021',
+    image: 'https://miro.medium.com/max/1400/0*IZ2xCECQfJ4tsYqs',
+    type: ResourceType.ARTICLES,
+    link: 'https://medium.com/nwplusubc/behind-cmd-f-2021-32ef6de26638'
+  },
+  {
+    name: 'Behind nwHacks 2021',
+    event: ResourceEvent.NW_HACKS,
+    year: '2021',
+    image: 'https://miro.medium.com/max/1400/1*cQTv2qABzHhzemU2l885kg.png',
+    type: ResourceType.ARTICLES,
+    link: 'https://medium.com/nwplusubc/behind-nwhacks-2021-947ede7ef2bd'
+  },
+  {
+    name: 'Behind HackCamp 2020: nwPlus’ first virtual hackathon',
+    event: ResourceEvent.HACK_CAMP,
+    year: '2020',
+    image: 'https://miro.medium.com/max/1400/0*f6s7xt91d0xTG-S-',
+    type: ResourceType.ARTICLES,
+    link: 'https://medium.com/nwplusubc/behind-hackcamp-2020-nwplus-first-virtual-hackathon-a5b95e50485e'
   },
 ];
 
