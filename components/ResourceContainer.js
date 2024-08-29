19e1cc95f05578089f28948a7d15e2dc182e3a64
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-08-28 17:35:22 -0700

    Remove unused const

--- components/ResourceContainer.js
+++ components/ResourceContainer.js
@@ -89,7 +89,6 @@ const RESOURCES_BODY =
   'If you are looking to get started in Computer Science, check out our ';
 const RESOURCES_LINK = 'https://resources.nwplus.io/';
 const RESOURCES_LINK_TEXT = 'Self-Learning Resources Wiki';
-const EXCLAMATION_MARK = '!';
 
 export default function ResourceContainer() {
   const [typeFilters, setTypeFilters] = useState({
